6878e962c6f52acdcda4b278ab70c175435fd986
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-09-06 03:24:39 +0900

    initial commit

---
+++ src/SessionProvider.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the Ray.AuraSessionModule package
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace Ray\AuraSessionModule;
+
+use Aura\Session\SessionFactory;
+use Ray\Di\ProviderInterface;
+
+class SessionProvider implements ProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function get()
+    {
+        return (new SessionFactory)->newInstance($_COOKIE);
+    }
+}
